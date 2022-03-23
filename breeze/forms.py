581149619514ac03b528095a8a75a430859e01d5
Vuong <vuongc@wit.edu>
2022-03-23 15:40:34 -0400

    created form for login

---
+++ breeze/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from breeze.models import Dreamreal
+
+class LoginForm(forms.Form):
+   user = forms.CharField(max_length = 100)
+   password = forms.CharField(widget = forms.PasswordInput())
+
+   def clean_message(self):
+      username = self.cleaned_data.get("username")
+      dbuser = Dreamreal.objects.filter(name = username)
+      
+      if not dbuser:
+         raise forms.ValidationError("User does not exist in the database")
+      return username
--- breeze/views.py
+++ breeze/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from breeze.forms import LoginForm
 
 def home(request):
     return render(request, 'account_creation.html')
+
+def login(request):
+   username = "not logged in"
+   
+   if request.method == "POST":
+      
+      MyLoginForm = LoginForm(request.POST)
+      
+      if MyLoginForm.is_valid():
+         username = MyLoginForm.cleaned_data['username']
+   else:
+      MyLoginForm = LoginForm()
+		
+   return render(request, 'loggedin.html', {"username" : username})
