2dcfd5e1c19c087e419a6f78065a825ba6b78693
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2022-03-23 17:15:42 -0400

    bugfix for db models

--- breeze/models.py
+++ breeze/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    user_id = models.IntField(min_length=1)
-    username = models.CharField(max_length=50,min_length=1)
-    password = models.CharField(max_length=50,min_length=1)
-    default_location = models.CharField(max_length=50,min_length=1)
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    default_location = models.CharField(max_length=50)
     
-class ShoppingList(models.Model,User):
-    list_id = models.IntField(min_length=1)
+class ShoppingList(models.Model):
+    list_id = models.IntegerField()
     userid = User.user_id
+    userid = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
     
-class Item(models.Model,ShoppingList): 
-    item_id = models.IntField(min_length=1)
-    listid = ShoppingList.list_id
-    item_name = models.CharField(max_length=50,min_length=1)
-    location = models.CharField(max_length=50,min_length=1)
-    
+class Item(models.Model): 
+    item_id = models.IntegerField()
+    userid = models.ForeignKey('ShoppingList', on_delete=models.SET_NULL, null=True)
+    item_name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
     
     
