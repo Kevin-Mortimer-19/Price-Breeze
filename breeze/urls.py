ecd0098280fbe9abcf2ab7f401f7d7cb9025697b
Ducharme <ducharmee@wit.edu>
2022-04-06 16:05:30 -0400

    reset password attempt 3

--- breeze/urls.py
+++ breeze/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 from django.views.generic import TemplateView
 from breeze import views
 
+app_name = "main"
+
 urlpatterns = [
     path("", views.create_account, name="create_account"),
     path("home/", views.home, name="home"),
     path("log_in/", views.login, name="login"),
+    path("password_reset", views.password_reset_request, name="password_reset"),
 ]
--- final/urls.py
+++ final/urls.py
@@ -18,13 +18,12 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 # from final import views
 
-app_name = "main" 
+
 
 urlpatterns = [
     path("", include("breeze.urls")),
     path('admin/', admin.site.urls),
   # path('accounts/', include('django.contrib.auth.urls')),
-    path("password_reset", auth_views.password_reset_request, name="password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),      
