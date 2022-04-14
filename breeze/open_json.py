40e0ef459c2281e66ec82453b0bc2b41108bf8d1
OuyaSusumu <vuongc@wit.edu>
2022-04-14 00:12:21 -0400

    turned table outputs to functions

--- breeze/open_json.py
+++ breeze/open_json.py
@@ -1,5 +1,9 @@
+from unicodedata import category
+import pandas
+import numpy
 import json
 from sort import *
+from IPython.display import HTML
 
 # Opening JSON file
 f = open('products.json')
@@ -8,9 +12,30 @@ f = open('products.json')
 # a list of dictionaries
 data = json.load(f)
 
-#sorted data by price
-data_sorted_greatest = sort_great_least_price(data)
-data_sorted_lowest = sort_least_great_price(data)
+def startTable():
+    df = pandas.DataFrame(data)
+    dfg = df.groupby(['title','category','price'], sort=False).sum()
+    table = dfg.to_html()
+
+    return table
+
+def highTable():
+#sorted data by price highest to lowest 
+    data_sorted_greatest = sort_great_least_price(data)
+    df = pandas.DataFrame(data_sorted_greatest)
+    dfg = df.groupby(['title','category','price'], sort=False).sum()
+    table = dfg.to_html()
+
+    return table
+
+def lowTable():
+#sorted data by price lowest to highest 
+    data_sorted_lowest = sort_least_great_price(data)
+    df = pandas.DataFrame(data_sorted_lowest)
+    dfg = df.groupby(['title','category','price'], sort=False).sum()
+    table = dfg.to_html()
+
+    return table
 
 # Closing file
 f.close()
