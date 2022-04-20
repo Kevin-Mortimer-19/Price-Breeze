583ec07672a209100568ce2e3088c48974d0988c
Sarah <mahmouds@wit.edu>
2022-04-20 17:03:05 -0400

    Revert "Db arch model"

--- breeze/admin.py
+++ breeze/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User, ShoppingList, Item
 
-admin.site.register(User)
+#admin.site.register(User)
+#admin.site.register(Profile)
 admin.site.register(ShoppingList)
 admin.site.register(Item)
